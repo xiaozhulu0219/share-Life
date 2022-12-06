6ea77bcab9b68718c49d4ca08dc67ebda18b6d9f
allentoup@ <allentoup@163.com>
2022-12-06 17:12:55 +0800

    改一下端口号

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -2,10 +2,13 @@ let BASE_URL = ''
 
 
 if (process.env.NODE_ENV == 'development') {
-    BASE_URL = 'http://43.142.29.190:9090/jeecg-boot' // 开发环境
+    BASE_URL = 'http://127.0.0.1:9090/jeecg-boot' // 开发环境
 } else {
-	BASE_URL = 'http://boot.jeecg.org:9090/jeecg-boot' // 生产环境
+	BASE_URL = 'http://43.142.29.190:9090/jeecg-boot' // 现在生产环境也用这个
 }
+// } else {
+// 	BASE_URL = 'http://boot.jeecg.org:9090/jeecg-boot' // 生产环境
+// }
 let staticDomainURL = BASE_URL + '/sys/common/static';
 
 const configService = {
