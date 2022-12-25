a22e7f7102eb6ac246a139c13b452a3d49202796
allentoup@ <allentoup@163.com>
2022-12-25 21:26:50 +0800

    修改端口

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -2,9 +2,10 @@ let BASE_URL = ''
 
 
 if (process.env.NODE_ENV == 'development') {
-    BASE_URL = 'http://43.142.29.190:9090/jeecg-boot' // 开发环境
+    //BASE_URL = 'http://127.0.0.1:7070/share-life' // 本地环境
+    BASE_URL = 'http://43.142.29.190:8080/share-life' // 开发环境
 } else {
-	BASE_URL = 'http://43.142.29.190:9090/jeecg-boot' // 现在生产环境也用这个
+	BASE_URL = 'http://43.142.29.190:8080/share-life' // 现在生产环境也用这个
 }
 // } else {
 // 	BASE_URL = 'http://boot.jeecg.org:9090/jeecg-boot' // 生产环境
