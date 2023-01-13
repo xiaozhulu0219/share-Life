def486af58a7ce4d3c022015f7e8dc15b5a2ff72
allentoup@ <allentoup@163.com>
2023-01-13 12:51:59 +0800

    修改

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -3,9 +3,9 @@ let BASE_URL = ''
 
 if (process.env.NODE_ENV == 'development') {
     //BASE_URL = 'http://127.0.0.1:7070/share-life' // 本地环境
-    BASE_URL = 'http://43.142.29.190:6060/share-life' // 开发环境
+    BASE_URL = 'http://43.143.124.217:6060/share-life' // 开发环境   6060 是 prod 环境
 } else {
-	BASE_URL = 'http://43.142.29.190:6060/share-life' // 现在生产环境也用这个
+	BASE_URL = 'http://43.143.124.217:6060/share-life' // 现在生产环境也用这个
 }
 // } else {
 // 	BASE_URL = 'http://boot.jeecg.org:9090/jeecg-boot' // 生产环境
