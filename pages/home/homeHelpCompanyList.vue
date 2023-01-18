5636a03afcff4b0993b8cdd3f85a9b6cd6d3d3fd
allentoup@ <allentoup@163.com>
2023-01-18 10:57:12 +0800

    首页助力页面

---
+++ pages/home/homeHelpCompanyList.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+1111
+
+	</view>
+</template>
+
+<script>
+	import MescrollMixin from "@/components/mescroll-uni/mescroll-mixins.js";
+	import Mixin from "@/common/mixin/Mixin.js";
+
+	export default {
+		name: 'MyHelpCompanyList',
+		mixins: [MescrollMixin, Mixin],
+		data() {
+			return {
+				findMyPublishComPageUrl: '/company/movements/findMyPublishComPage'
+			};
+		},
+		created() {
+			this.getMyHelpCompanyList();
+		},
+		methods: {
+			getMyHelpCompanyList() {
+				this.$http.get(this.findMyPublishComPageUrl, {
+					params: {
+						page: 1,
+						pagesize: 20
+					}
+				}).then(res => {
+					if (res.data.success) {
+						console.log(res.data.result);
+					}
+				}).catch(err => {
+					console.log(err);
+				});
+			}
+		}
+	}
+</script>
+
+<style lang='scss'>
+
+</style>
