94321c72c359e4b4a9bd109a08db6b29a9278396
allentoup@ <allentoup@163.com>
2023-02-06 14:12:43 +0800

    加一些注释

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -7,9 +7,7 @@ if (process.env.NODE_ENV == 'development') {
 } else {
 	BASE_URL = 'http://43.143.124.217:6060/share-life' // 现在生产环境也用这个
 }
-// } else {
-// 	BASE_URL = 'http://boot.jeecg.org:9090/jeecg-boot' // 生产环境
-// }
+
 let staticDomainURL = BASE_URL + '/sys/common';
 // http://43.143.124.217:6060/share-life/sys/common/static/1_1673882232390.jpg 图片拼接示例
 const configService = {
