cb9ed1a18d47abc457c61a4f04fc5d050b62d704
allentoup@ <allentoup@163.com>
2023-04-17 11:43:17 +0800

    修改ip

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -3,13 +3,13 @@ let BASE_URL = ''
 
 if (process.env.NODE_ENV == 'development') {
     //BASE_URL = 'http://127.0.0.1:7070/share-life' // 本地环境
-    BASE_URL = 'http://43.143.124.217:6060/share-life' // 开发环境
+    BASE_URL = 'http://101.43.131.189:6060/share-life' // 开发环境
 } else {
-	BASE_URL = 'http://43.143.124.217:6060/share-life' // 现在生产环境也用这个
+	BASE_URL = 'http://101.43.131.189:6060/share-life' // 现在生产环境也用这个
 }
 
 let staticDomainURL = BASE_URL + '/sys/common';
-// http://43.143.124.217:6060/share-life/sys/common/static/1_1673882232390.jpg 图片拼接示例
+// http://101.43.131.189:6060/share-life/sys/common/static/1_1673882232390.jpg 图片拼接示例
 const configService = {
 	apiUrl: BASE_URL,
 	staticDomainURL: staticDomainURL
