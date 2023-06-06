c2677fc500237386f8d9f8d78c99142a56209095
allentoup@ <allentoup@163.com>
2023-06-06 11:48:18 +0800

    删除home中一些不再用到的接口，样式等

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -48,13 +48,7 @@
                 activeTab: {},
                 CustomBar: this.CustomBar,
                 NavBarColor: this.NavBarColor,
-                url: '/umsMember/list',
-                findHomePublishComListUrl: '/company/findHomePublishComList',
-                findHomePublishInforListUrl: '/information/movements/findHomePublishInforList',
-                homePublishComList: [],
-                homePublishInforList: [],
                 inputValue: '',
-                fileUrl: configService.fileSaveURL ,
                 searchHistoryList: [{
                     locationName: '反而可能',
                     createTime: '2022-11-30 10:00:00',
@@ -77,59 +71,12 @@
                     createBy: '之乎者也有限公司',
                     status: 2
                 }], //搜索出来的内容(假数据)
-                downOption: {
-                    use: true, // 是否启用下拉刷新; 默认true
-                    auto: true, // 是否在初始化完毕之后自动执行下拉刷新的回调; 默认true
-                    native: false, // 是否使用系统自带的下拉刷新; 默认false; 仅mescroll-body生效 (值为true时,还需在pages配置enablePullDownRefresh:true;详请参考mescroll-native的案例)
-                    autoShowLoading: false, // 如果设置auto=true(在初始化完毕之后自动执行下拉刷新的回调),那么是否显示下拉刷新的进度; 默认false
-                    isLock: false, // 是否锁定下拉刷新,默认false;
-                    offset: 60, // 在列表顶部,下拉大于80upx,松手即可触发下拉刷新的回调
-                    inOffsetRate: 1, // 在列表顶部,下拉的距离小于offset时,改变下拉区域高度比例;值小于1且越接近0,高度变化越小,表现为越往下越难拉
-                    outOffsetRate: 0.2, // 在列表顶部,下拉的距离大于offset时,改变下拉区域高度比例;值小于1且越接近0,高度变化越小,表现为越往下越难拉
-                    bottomOffset: 20, // 当手指touchmove位置在距离body底部20upx范围内的时候结束上拉刷新,避免Webview嵌套导致touchend事件不执行
-                    minAngle: 45, // 向下滑动最少偏移的角度,取值区间  [0,90];默认45度,即向下滑动的角度大于45度则触发下拉;而小于45度,将不触发下拉,避免与左右滑动的轮播等组件冲突;
-                    bgColor: "#E75A7C", // 背景颜色 (建议在pages.json中再设置一下backgroundColorTop)
-                    textColor: "#fff", // 文本颜色 (当bgColor配置了颜色,而textColor未配置时,则textColor会默认为白色)
-                    textInOffset: '下拉刷新', // 下拉的距离在offset范围内的提示文本
-                    textOutOffset: '释放更新', // 下拉的距离大于offset范围的提示文本
-                    textLoading: '稍等加载中 ...'    // 加载中的提示文本
-                },
-                upOption: {
-                    auto: false, // 不自动加载
-                    page: {
-                        num: 0, // 当前页码,默认0,回调之前会加1,即callback(page)会从1开始
-                        size: 6 // 每页数据的数量
-                    },
-                    // noMoreSize: 6, //如果列表已无数据,可设置列表的总数量要大于半页才显示无更多数据;避免列表数据过少(比如只有一条数据),显示无更多数据会不好看; 默认5
-                    empty: {
-                        tip: '~ 空空如也 ~', // 提示
-                        btnText: '去逛逛 >', // 按钮
-                        use: true, // 是否显示空布局
-                        icon: "https://www.mescroll.com/img/mescroll-empty.png", // 图标路径
-                        fixed: false, // 是否使用fixed定位,默认false; 配置fixed为true,以下的top和zIndex才生效 (transform会使fixed失效,最终会降级为absolute)
-                        top: "100rpx", // fixed定位的top值 (完整的单位值,如 "10%"; "100rpx")
-                        zIndex: 99 // fixed定位z-index值
-                    },
-                    toTop: {
-                        // 回到顶部按钮,需配置src才显示
-                        src: "https://www.mescroll.com/img/mescroll-totop.png", // 图片路径
-                        offset: 1000, // 列表滚动多少距离才显示回到顶部按钮,默认1000
-                        duration: 300, // 回到顶部的动画时长,默认300ms (当值为0或300则使用系统自带回到顶部,更流畅; 其他值则通过step模拟,部分机型可能不够流畅,所以非特殊情况不建议修改此项)
-                        zIndex: 9990, // fixed定位z-index值
-                        left: null, // 到左边的距离, 默认null. 此项有值时,right不生效. (支持20, "20rpx", "20px", "20%"格式的值, 其中纯数字则默认单位rpx)
-                        right: 20, // 到右边的距离, 默认20 (支持20, "20rpx", "20px", "20%"格式的值, 其中纯数字则默认单位rpx)
-                        bottom: 120, // 到底部的距离, 默认120 (支持20, "20rpx", "20px", "20%"格式的值, 其中纯数字则默认单位rpx)
-                        safearea: false, // bottom的偏移量是否加上底部安全区的距离, 默认false, 需要适配iPhoneX时使用 (具体的界面如果不配置此项,则取mescroll组件props的safearea值)
-                        width: 72, // 回到顶部图标的宽度, 默认72 (支持20, "20rpx", "20px", "20%"格式的值, 其中纯数字则默认单位rpx)
-                        radius: "50%" // 圆角, 默认"50%" (支持20, "20rpx", "20px", "20%"格式的值, 其中纯数字则默认单位rpx)
-                    },
-                },
             };
         },
-        created() {
-            this.getHomePublishComList();
-            this.getHomePublishInforList();
-        },
+        // created() {
+        //     this.getHomePublishComList();
+        //     this.getHomePublishInforList();
+        // },
         methods: {
             getActiveTab(item) {
                 this.activeTab = item;
@@ -138,56 +85,11 @@
             handleStatus(status, type) {
 
             },
-            //标签为"助力"时展示的特殊数据
-            getHomePublishComList() {
-                this.$http.get(this.findHomePublishComListUrl, {
-                    params: {
-                        page: 1,
-                        pagesize: 20
-                    }
-                }).then(res => {
-                    if (res.data.success) {
-                        console.log(res.data.result);
-                        this.homePublishComList = res.data.result.items;
-                    }
-                }).catch(err => {
-                    console.log(err);
-                });
-            },
-            //标签为"助力"之外的标签展示的数据--以后将考虑根据类型type来区分调用不同类型的接口，展示在不同标签的列表页
-            getHomePublishInforList() {
-                this.$http.get(this.findHomePublishInforListUrl, {
-                    params: {
-                        page: 1,
-                        pagesize: 20
-                    }
-                }).then(res => {
-                    if (res.data.success) {
-                        console.log("res.data.result.items！！",res.data.result.items);
-                        this.homePublishInforList = res.data.result.items;
-                    }
-                }).catch(err => {
-                    console.log(err);
-                });
-            },
-
             goHome() {
                 this.$Router.push({
                     name: 'index'
                 });
             },
-            /*	search() {
-                        //表单项内容发生改变-- - 助力详情页
-                        uni.request({
-                            url: "company/movements/findPagePublishByRecommend",
-                            success(res) {
-                                console.log(res)
-                            }
-                        })
-
-
-
-                    },*/
             model(item, index) {
                 this.inputValue = item;
             },
@@ -238,9 +140,7 @@
                 let list = await uni.getStorage({
                     key: 'searchList'
                 });
-
                // console.log(list[1].data);
-
                 // if (list[1].data) {
                 //     this.searchHistoryList = JSON.parse(list[1].data);
                 // }
@@ -299,130 +199,5 @@
                 margin: 6rpx 5rpx;
             }
         }
-    }
-
-    .main_classify {
-        background: white;
-
-        .main_under_classify {
-            .li {
-                height: 124rpx;
-                border-bottom: 2rpx #999999 solid;
-                padding: 20rpx 28rpx;
-                display: flex;
-                justify-content: space-between;
-                margin-top: 20rpx;
-
-                image {
-                    width: 116rpx;
-                    height: 110rpx;
-                    border-radius: 50%;
-                    margin-right: 20rpx;
-                    margin-top: 6rpx;
-                }
-
-                .li_content {
-                    width: 254rpx;
-
-                    .title {
-                        display: block;
-                        font-weight: 800;
-                        font-size: 28rpx;
-                    }
-
-                    text {
-                        line-height: 40rpx;
-                    }
-
-                    .zhiwei {
-                        color: #666666;
-                    }
-
-                    .heng {
-                        color: #999999;
-                    }
-                }
-
-                .li_end {
-                    padding: 0rpx 40rpx;
-                    width: 140rpx;
-                    height: 52rpx;
-                    border: 1rpx solid red;
-                    border-radius: 50rpx;
-                    display: flex;
-                    justify-content: space-between;
-                    margin-top: 36rpx;
-
-                    .jia {
-                        width: 24rpx;
-                        height: 24rpx;
-                        margin-top: 16rpx;
-                    }
-
-                    .erji {
-                        width: 42rpx;
-                        height: 42rpx;
-                    }
-                }
-            }
-        }
-    }
-
-    .tab-content {
-        background-color: #00a8cc;
-        position: fixed;
-        width: 100%;
-        height: 200rpx;
-        top: 260rpx;
-    }
-
-    .list-item {
-        margin: 20rpx auto;
-        width: 95%;
-        border-radius: 20rpx;
-    }
-
-    .card {
-        background-color: $uni-bg-color-grey;
-        padding: 20rpx 20rpx;
-        border-radius: 20rpx;
-        margin-bottom: 10rpx;/*盒子间的距离*/
-        margin-top: 100rpx; /*盒子距离顶部的距离*/
-        line-height: 35rpx;  /*行高*/
-        //margin-bottom: 16px; /*内容和标题间的间距*/
-
-        .card-title {
-            font-weight: bold;
-        }
-
-        .card-text {
-            font-size: 20rpx;
-        }
-
-        .card-location {
-            position: absolute;
-            right: 20rpx;
-            font-size: 20rpx;
-        }
-    }
-
-    .medias_size {
-        max-width: 180px;
-        width: 180px;
-        width:expression(this.width > 180 ? "180px" : this.width);
-        height: 180px;
-        height:expression(this.height > 180 ? "180px" : this.height);
-        overflow:hidden;
-        /*text-align:center;*/
-        /*width: 21rpx;*/
-        /*height: 21rpx;*/
-        /*border-radius: 8rpx;*/
-    }
-
-
-    .icon {
-        width: 36rpx;
-        height: 36rpx;
-        margin-right: 14rpx;
     }
 </style>
