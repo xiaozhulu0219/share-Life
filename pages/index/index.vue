bd90fc37b5ef24a50eb090b69652da0167a9306c
allentoup <allentoup@163.com>
2023-06-27 21:49:44 +0800

    调整样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,62 +1,63 @@
 <template>
-	<view>
-		<home :cur="PageCur" v-if="PageCur=='home'" :key="commponent1Key"></home>
-<!--		<information :cur="PageCur" v-if="PageCur == 'information'" :key="commponent3Key"></information>-->
-		<member v-if="PageCur == 'member'" :key="commponent4Key"></member>
-		<publish v-if="PageCur == 'publish'" :key="commponent4Key"></publish>
-		<message v-if="PageCur == 'message'" :key="commponent2Key"></message>
-		<view class="cu-bar tabbar bg-white shadow foot">
-			<view :class="PageCur == 'home' ? 'action text-green' : 'action text-gray'" @click="NavChange" data-cur="home">
-				<view class="cuIcon-homefill"></view>
-				首页
-			</view>
-			<view :class="PageCur == 'publish' ? 'action text-green' : 'action text-gray'" @click="showModal" data-cur="publish">
-				<view class="cuIcon-roundaddfill"></view>
-			</view>
-			<view :class="PageCur == 'message' ? 'action text-blue' : 'action text-gray'" @click="NavChange" data-cur="message">
-				<view class="cuIcon-message"></view>
-				消息
-			</view>
-			<view :class="PageCur == 'member' ? 'action text-blue' : 'action text-gray'" @click="NavChange" data-cur="member">
-				<view class="cuIcon-people"></view>
-				我的
-			</view>
-		</view>
-		<ToPublishPopup ref='toPublishPopup'></ToPublishPopup>
-	</view>
+    <view>
+        <home :cur="PageCur" v-if="PageCur=='home'" :key="commponent1Key"></home>
+        <!--		<information :cur="PageCur" v-if="PageCur == 'information'" :key="commponent3Key"></information>-->
+        <member v-if="PageCur == 'member'" :key="commponent4Key"></member>
+        <publish v-if="PageCur == 'publish'" :key="commponent4Key"></publish>
+        <message v-if="PageCur == 'message'" :key="commponent2Key"></message>
+        <view class="cu-bar tabbar bg-white shadow foot">
+            <view :class="PageCur == 'home' ? 'action text-green' : 'action text-gray'" @click="NavChange" data-cur="home">
+                <view class="cuIcon-homefill"></view>
+                首页
+            </view>
+            <view :class="PageCur == 'publish' ? 'action text-green' : 'action text-gray'" @click="showModal" data-cur="publish">
+                <view class="cuIcon-roundaddfill"></view>
+            </view>
+            <view :class="PageCur == 'message' ? 'action text-blue' : 'action text-gray'" @click="NavChange" data-cur="message">
+                <view class="cuIcon-message"></view>
+                消息
+            </view>
+            <view :class="PageCur == 'member' ? 'action text-blue' : 'action text-gray'" @click="NavChange" data-cur="member">
+                <view class="cuIcon-people"></view>
+                我的
+            </view>
+        </view>
+        <ToPublishPopup ref='toPublishPopup'></ToPublishPopup>
+    </view>
 </template>
 
 <script>
-	import ToPublishPopup from '../publish/toPublishPopup.vue'
-export default {
-	data() {
-		return {
-			PageCur: 'home',
-			commponent1Key: 0,
-			commponent2Key: 0,
-			commponent3Key: 0,
-			commponent4Key: 0
-		};
-	},
-	components:{
-		ToPublishPopup
-	},
-	onLoad: function() {
-		this.PageCur = 'home';
-		++this.commponent1Key;
-		++this.commponent2Key;
-		++this.commponent3Key;
-		++this.commponent4Key;
-	},
-	methods: {
-		showModal(){
-			this.$refs.toPublishPopup.showModal();
-		},
-		NavChange: function(e) {
-			this.PageCur = e.currentTarget.dataset.cur;
-		},
-	}
-};
+    import ToPublishPopup from '../publish/toPublishPopup.vue'
+
+    export default {
+        data() {
+            return {
+                PageCur: 'home',
+                commponent1Key: 0,
+                commponent2Key: 0,
+                commponent3Key: 0,
+                commponent4Key: 0
+            };
+        },
+        components: {
+            ToPublishPopup
+        },
+        onLoad: function () {
+            this.PageCur = 'home';
+            ++this.commponent1Key;
+            ++this.commponent2Key;
+            ++this.commponent3Key;
+            ++this.commponent4Key;
+        },
+        methods: {
+            showModal() {
+                this.$refs.toPublishPopup.showModal();
+            },
+            NavChange: function (e) {
+                this.PageCur = e.currentTarget.dataset.cur;
+            },
+        }
+    };
 </script>
 
 <style scoped>
--- pages/message/message.vue
+++ pages/message/message.vue
@@ -22,6 +22,7 @@
 
 
     export default {
+        name: 'message',
         mixins: [MescrollMixin, Mixin, MescrollMoreMixin],
         components: {
 
