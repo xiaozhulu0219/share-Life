f8053fa25df9821d47034a1adf21063364dd1c4a
allentoup@ <allentoup@163.com>
2023-07-12 15:19:57 +0800

    格式化了一下憧憬公司标签页面和性别页面，删除了一些无用的东西

--- pages/member/dreamCompanySign.vue
+++ pages/member/dreamCompanySign.vue
@@ -1,12 +1,12 @@
 <template>
-	<!--个人页的一些页面--憧憬公司标签页-->
+    <!--个人页的一些页面--憧憬公司标签页-->
     <view>
-      <view @tap="saveTags">
-        <cu-custom bgColor="bg-gradual-pink" :isBack="true">
-            <block slot="backText">返回</block>
-            <block slot="content">憧憬公司标签</block>
-        </cu-custom>
-      </view>
+        <view @tap="saveTags">
+            <cu-custom bgColor="bg-gradual-pink" :isBack="true">
+                <block slot="backText">返回</block>
+                <block slot="content">憧憬公司标签</block>
+            </cu-custom>
+        </view>
         <!-- <form>
             <view class="cu-form-group">
                 <view class="title">憧憬公司标签</view>
@@ -17,169 +17,168 @@
                 <button class="cu-btn bg-blue lg" @click="onSubmit">提交</button>
             </view>
         </form> -->
-		<view class="content-box padding bg-white">
-			<view class="">
-				<view class="padding-bottom text-bold">
-					已选择标签
-				</view>
-				<view class="margin-bottom flex flex-wrap" v-if="selectedList.length > 0">
-					<text class="tag-item active" v-for="(item,index) in selectedList" :key="index">
-						{{item}}
-					</text>
-				</view>
-				<view class="margin-bottom" v-else>
-					暂无
-				</view>
-			</view>
-			<view class="">
-				<view class="padding-bottom text-bold">自定义标签</view>
-				<view class="margin-bottom flex flex-wrap">
-					<template v-if="customList.length">
-						<text :class='["tag-item", { "active": item.status }]' v-for="(item,index) in customList" :key="index" @tap="selectTag(item)">{{item.selfComSign}}</text>
-					</template>
-					<text class="tag-item" @tap="addTag">
-						+
-					</text>
-				</view>
-			</view>
-			<view class="">
-				<view class="padding-bottom text-bold">
-					憧憬公司标签
-				</view>
-				<view class="margin-bottom flex flex-wrap" v-if="tagList.length > 0">
-					<text :class='["tag-item", { "active": item.status }]' v-for="(item,index) in tagList" :key="index" @tap="selectTag(item)">
-						{{item.label}}
-					</text>
-				</view>
-				<view class="margin-bottom" v-else>
-					暂无
-				</view>
-			</view>
-		</view>
-    <!-- 自定义标签模态框 -->
-    <view class="customTag-mask" v-if="showCustomModal">
-      <view class="customTag-box">
-        <view class="tag-title">自定义标签</view>
-        <input type="text" class="tag-input" v-model="tagName" maxlength="6"
-        placeholder="最长6个字" placeholder-color="#999"/>
-        <view class="buttons">
-          <view class="btn" @tap.stop="showCustomModal = false">取消</view>
-          <view class="btn-confirm btn" @tap="getCustomTagConfirm">保存</view>
+        <view class="content-box padding bg-white">
+            <view class="">
+                <view class="padding-bottom text-bold">
+                    已选择标签
+                </view>
+                <view class="margin-bottom flex flex-wrap" v-if="selectedList.length > 0">
+                    <text class="tag-item active" v-for="(item,index) in selectedList" :key="index">
+                        {{item}}
+                    </text>
+                </view>
+                <view class="margin-bottom" v-else>
+                    暂无
+                </view>
+            </view>
+            <view class="">
+                <view class="padding-bottom text-bold">自定义标签</view>
+                <view class="margin-bottom flex flex-wrap">
+                    <template v-if="customList.length">
+                        <text :class='["tag-item", { "active": item.status }]' v-for="(item,index) in customList"
+                              :key="index" @tap="selectTag(item)">{{item.selfComSign}}
+                        </text>
+                    </template>
+                    <text class="tag-item" @tap="addTag">
+                        +
+                    </text>
+                </view>
+            </view>
+            <view class="">
+                <view class="padding-bottom text-bold">
+                    憧憬公司标签
+                </view>
+                <view class="margin-bottom flex flex-wrap" v-if="tagList.length > 0">
+                    <text :class='["tag-item", { "active": item.status }]' v-for="(item,index) in tagList" :key="index"
+                          @tap="selectTag(item)">
+                        {{item.label}}
+                    </text>
+                </view>
+                <view class="margin-bottom" v-else>
+                    暂无
+                </view>
+            </view>
         </view>
-      </view>
-    </view>
+        <!-- 自定义标签模态框 -->
+        <view class="customTag-mask" v-if="showCustomModal">
+            <view class="customTag-box">
+                <view class="tag-title">自定义标签</view>
+                <input type="text" class="tag-input" v-model="tagName" maxlength="6"
+                       placeholder="最长6个字" placeholder-color="#999"/>
+                <view class="buttons">
+                    <view class="btn" @tap.stop="showCustomModal = false">取消</view>
+                    <view class="btn-confirm btn" @tap="getCustomTagConfirm">保存</view>
+                </view>
+            </view>
+        </view>
     </view>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      index: -1,
-      imgList: [],
-      uploadUrl: '/sys/common/upload',
-      myFormData: {
-          dreamCompanySign: '',
-          sex: 1,
-          orgCode: '',
-          workNo: '',
-          id: ''
-      },
-      selectedList: [], //选中标签列表
-      customList: [], // 自定义标签列表
-      tagList: [], // 默认官方标签列表
-      tagName: '', // 自定义标签名
-      showCustomModal: false // 控制自定义标签模态框显示
+    export default {
+        data() {
+            return {
+                index: -1,
+                myFormData: {
+                    dreamCompanySign: '',
+                    sex: 1,
+                    id: ''
+                },
+                selectedList: [], //选中标签列表
+                customList: [], // 自定义标签列表
+                tagList: [], // 默认官方标签列表
+                tagName: '', // 自定义标签名
+                showCustomModal: false // 控制自定义标签模态框显示
+            };
+        },
+        onLoad: function (option) {
+            this.queryTags(this.$Route.query);
+        },
+        methods: {
+            handleTagSelect(tags) {
+                const tagSelectArr = tags.split(',');
+                const customName = this.customList.map(item => item.label);
+                const tagName = this.tagList.map(item => item.label);
+                tagSelectArr.forEach(item => {
+                    const cIndex = customName.indexOf(item);
+                    const tIndex = tagName.indexOf(item);
+                    if (cIndex !== -1) {
+                        this.selectStatus(this.customList[cIndex], cIndex);
+                    }
+                    if (tIndex !== -1) {
+                        this.selectStatus(this.tagList[tIndex], tIndex);
+                    }
+                });
+            },
+            // 保存所选标签
+            saveTags() {
+                const params = {
+                    dreamCompanySign: this.selectedList.join(','),
+                    id: this.$store.getters.userid
+                };
+                this.$http.get('/sys/editDreamCompanySign', {params}).then(res => {
+                }).catch(e => {
+                    console.log('请求错误', e);
+                });
+            },
+            queryTags(tags) {
+                this.$http.get('/sys/dict/querySomeDictItems', {params: {dicts: 'member_dream_company_sign'}}).then(res => {
+                    if (res.data.success) {
+                        const newList = res.data.result.member_dream_company_sign.map(item => {
+                            item.status = false;
+                            return item;
+                        });
+                        this.tagList = newList;
+                    }
+                });
+                this.getCustomTag(tags);
+            },
+            selectTag(item) {
+                const idx = this.selectedList.indexOf(item.label);
+                this.selectStatus(item, idx);
+            },
+            // 选中状态
+            selectStatus(item, idx) {
+                if (item.status) {
+                    item.status = false;
+                    this.selectedList.splice(idx, 1);
+                } else {
+                    item.status = true;
+                    this.selectedList.push(item.label);
+                }
+            },
+            addTag() {
+                this.showCustomModal = true;
+            },
+            // 新增自定义标签
+            getCustomTagConfirm() {
+                const {tagName, $http} = this;
+                if (!tagName.trim()) {
+                    this.$tip.toast('请输入标签');
+                    return;
+                }
+                const params = {selfComSign: tagName, id: this.$store.getters.userid};
+                $http.get('/sys/addSelfCompanySignService', {params}).then(res => {
+                    if (res.data.success) {
+                        this.showCustomModal = false;
+                        this.$tip.success('添加成功');
+                        this.getCustomTag();
+                    }
+                });
+            },
+            // 获取自定义标签
+            getCustomTag(tags) {
+                this.$http.get('/selfCompanySign/querySelfComSignByUserId',
+                    {params: {id: this.$store.getters.userid}}).then(res => {
+                    if (res.data.success) {
+                        const customList = res.data.result.map(item => ({...item, label: item.selfComSign}));
+                        this.customList = customList;
+                        this.handleTagSelect(tags);
+                    }
+                });
+            }
+        }
     };
-  },
-  onLoad: function(option) {
-		this.queryTags(this.$Route.query);
-  },
-  methods: {
-    handleTagSelect(tags) {
-      const tagSelectArr = tags.split(',');
-      const customName = this.customList.map(item => item.label);
-      const tagName = this.tagList.map(item => item.label);
-      tagSelectArr.forEach(item => {
-        const cIndex = customName.indexOf(item);
-        const tIndex = tagName.indexOf(item);
-        if (cIndex !== -1) {
-          this.selectStatus(this.customList[cIndex], cIndex);
-        }
-        if (tIndex !== -1) {
-          this.selectStatus(this.tagList[tIndex], tIndex);
-        }
-      });
-    },
-    // 保存所选标签
-    saveTags() {
-      const params = {
-        dreamCompanySign: this.selectedList.join(','),
-        id: this.$store.getters.userid
-      };
-      this.$http.get('/sys/editDreamCompanySign', { params }).then(res => {
-      }).catch(e => {
-				console.log('请求错误', e);
-			});
-    },
-		queryTags(tags) {
-      this.$http.get('/sys/dict/querySomeDictItems', { params: { dicts: 'member_dream_company_sign' } }).then(res => {
-        if (res.data.success) {
-          const newList = res.data.result.member_dream_company_sign.map(item => {
-            item.status = false;
-            return item;
-          });
-          this.tagList = newList;
-        }
-      });
-      this.getCustomTag(tags);
-		},
-		selectTag(item) {
-      const idx = this.selectedList.indexOf(item.label);
-      this.selectStatus(item, idx);
-		},
-    // 选中状态
-    selectStatus(item, idx) {
-      if (item.status) {
-        item.status = false;
-        this.selectedList.splice(idx, 1);
-      } else {
-       item.status = true;
-       this.selectedList.push(item.label);
-      }
-    },
-		addTag() {
-     this.showCustomModal = true;
-		},
-    // 新增自定义标签
-    getCustomTagConfirm() {
-      const { tagName, $http } = this;
-      if (!tagName.trim()) {
-        this.$tip.toast('请输入标签');
-        return;
-      }
-      const params = { selfComSign: tagName, id: this.$store.getters.userid };
-      $http.get('/sys/addSelfCompanySignService', { params }).then(res => {
-        if (res.data.success) {
-          this.showCustomModal = false;
-          this.$tip.success('添加成功');
-          this.getCustomTag();
-        }
-      });
-    },
-    // 获取自定义标签
-    getCustomTag(tags) {
-      this.$http.get('/selfCompanySign/querySelfComSignByUserId',
-        { params: { id: this.$store.getters.userid } }).then(res => {
-        if (res.data.success) {
-          const customList = res.data.result.map(item => ({ ...item, label: item.selfComSign }));
-          this.customList = customList;
-          this.handleTagSelect(tags);
-        }
-      });
-    }
-  }
-};
 </script>
 
 <style scoped>
@@ -187,65 +186,72 @@ export default {
         min-width: calc(4em + 15px);
     }
 
-	.content-box {
-		height: calc(100vh - 90rpx);
-	}
+    .content-box {
+        height: calc(100vh - 90rpx);
+    }
 
-	.tag-item {
-		margin: 10rpx;
-		padding: 16rpx 30rpx;
-		background-color: #ddd;
-		border-radius: 50rpx;
-		color: #fff;
-	}
+    .tag-item {
+        margin: 10rpx;
+        padding: 16rpx 30rpx;
+        background-color: #ddd;
+        border-radius: 50rpx;
+        color: #fff;
+    }
 
-	.tag-item.active {
-		background-color: #8874ff;
-	}
-  .customTag-mask {
-    background-color: rgba(0,0,0,.3);
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-  }
-  .customTag-box {
-    margin: auto;
-    padding-top: 20rpx;
-    width: 80%;
-    overflow: hidden;
-    text-align: center;
-    background-color: #fff;
-    border-radius: 20rpx;
-  }
-  .tag-title {
-    font-size: 18px;
-    color: #333;
-  }
-  .tag-input {
-    background-color: #f9f9f9;
-    margin: 20rpx 20rpx 60rpx;
-    padding: 4rpx 10rpx;
-    box-sizing: border-box;
-    height: 80rpx;
-    text-align: left;
-  }
-  .buttons {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .btn {
-    flex: 1;
-    height: 80rpx;
-    line-height: 80rpx;
-    color: #666;
-    background-color: #f0f0f0;
-  }
-  .btn-confirm {
-    background-color: #0081ff;
-    color: #fff;
-  }
+    .tag-item.active {
+        background-color: #8874ff;
+    }
+
+    .customTag-mask {
+        background-color: rgba(0, 0, 0, .3);
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+    }
+
+    .customTag-box {
+        margin: auto;
+        padding-top: 20rpx;
+        width: 80%;
+        overflow: hidden;
+        text-align: center;
+        background-color: #fff;
+        border-radius: 20rpx;
+    }
+
+    .tag-title {
+        font-size: 18px;
+        color: #333;
+    }
+
+    .tag-input {
+        background-color: #f9f9f9;
+        margin: 20rpx 20rpx 60rpx;
+        padding: 4rpx 10rpx;
+        box-sizing: border-box;
+        height: 80rpx;
+        text-align: left;
+    }
+
+    .buttons {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .btn {
+        flex: 1;
+        height: 80rpx;
+        line-height: 80rpx;
+        color: #666;
+        background-color: #f0f0f0;
+    }
+
+    .btn-confirm {
+        background-color: #0081ff;
+        color: #fff;
+    }
 </style>
--- pages/member/sex.vue
+++ pages/member/sex.vue
@@ -1,104 +1,114 @@
 <template>
-	<!--个人页的一些页面--性别修改页-->
-	<view class="sex-container">
-		<cu-custom bgColor="bg-gradual-pink" :isBack="true">
-			<view slot="backText">返回</view>
-			<view slot="content">编辑性别</view>
-			<view slot="right" @click="onSubmit()">保存</view>
-		</cu-custom>
-		<form>
-      <view class="title">选择你的性别</view>
-			<view class="sex-options">
-          <view @tap="switchSex(2)" class="sex">女<view v-show="sex === 2" class='cuIcon-check'></view></view>
-          <view class="gap"></view>
-          <view @tap="switchSex(1)" class="sex">男<view v-show="sex === 1" class='cuIcon-check'></view></view>
-			</view>
-		</form>
-	</view>
+    <!--个人页的一些页面--性别修改页-->
+    <view class="sex-container">
+        <cu-custom bgColor="bg-gradual-pink" :isBack="true">
+            <view slot="backText">返回</view>
+            <view slot="content">编辑性别</view>
+            <view slot="right" @click="onSubmit()">保存</view>
+        </cu-custom>
+        <form>
+            <view class="title">选择你的性别</view>
+            <view class="sex-options">
+                <view @tap="switchSex(2)" class="sex">女
+                    <view v-show="sex === 2" class='cuIcon-check'></view>
+                </view>
+                <view class="gap"></view>
+                <view @tap="switchSex(1)" class="sex">男
+                    <view v-show="sex === 1" class='cuIcon-check'></view>
+                </view>
+            </view>
+        </form>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				index: -1,
-        sex: 0
-			};
-		},
-		onLoad: function(option) {
-			const sex = this.$Route.query;
-      this.sex = sex;
-		},
-		methods: {
-      switchSex(sex) {
-        this.sex = sex;
-      },
-			onSubmit() {
-				const id = this.$store.getters.userid;
-				this.$tip.loading();
-				this.$http.get('/sys/editSex', {
-					params: {
-						id,
-						sex: this.sex
-					}
-				}).then(res => {
-					this.$tip.loaded();
-					if (res.data.success) {
-						this.$tip.toast('提交成功');
-						this.$Router.replace({
-							name: 'memberdetail'
-						});
-						/* uni.navigateTo({
-						    url: '/pages/user/userdetail'
-						}) */
-					}
-				}).catch((err) => {
-					console.log(err, 'err');
-					this.$tip.loaded();
-					this.$tip.error('提交失败');
-				});
-			}
-		}
-	};
+    export default {
+        data() {
+            return {
+                index: -1,
+                sex: 0
+            };
+        },
+        onLoad: function (option) {
+            const sex = this.$Route.query;
+            this.sex = sex;
+        },
+        methods: {
+            switchSex(sex) {
+                this.sex = sex;
+            },
+            onSubmit() {
+                const id = this.$store.getters.userid;
+                this.$tip.loading();
+                this.$http.get('/sys/editSex', {
+                    params: {
+                        id,
+                        sex: this.sex
+                    }
+                }).then(res => {
+                    this.$tip.loaded();
+                    if (res.data.success) {
+                        this.$tip.toast('提交成功');
+                        this.$Router.replace({
+                            name: 'memberdetail'
+                        });
+                        /* uni.navigateTo({
+                            url: '/pages/user/userdetail'
+                        }) */
+                    }
+                }).catch((err) => {
+                    console.log(err, 'err');
+                    this.$tip.loaded();
+                    this.$tip.error('提交失败');
+                });
+            }
+        }
+    };
 </script>
 
 <style>
-.right {
-  cursor: pointer;
-}
-.sex-container {
-  background-color: #f0f0f0;
-}
-	 .title {
-		min-width: calc(4em + 15px);
-    color: #666;
-    font-size: 16px;
-    margin-left: 20px;
-    margin-top: 20px;
+    .right {
+        cursor: pointer;
+    }
 
-	}
-  .sex-options {
-    box-sizing: border-box;
-    width: 96%;
-    margin: 20rpx;
-    height: 200rpx;
-    background-color: #fff;
-    border-radius: 16rpx;
-  }
-  .sex {
-    display: flex;
-    padding: 0 20px;
-    justify-content: space-between;
-    line-height: 100rpx;
-    cursor: pointer;
-  }
-  .cuIcon-check {
-    color:#0081ff;
-  }
-  .gap{
-    width: 98%;
-    height: 1px;
-    margin: 0 auto;
-    background: #f0f0f0;
-  }
+    .sex-container {
+        background-color: #f0f0f0;
+    }
+
+    .title {
+        min-width: calc(4em + 15px);
+        color: #666;
+        font-size: 16px;
+        margin-left: 20px;
+        margin-top: 20px;
+
+    }
+
+    .sex-options {
+        box-sizing: border-box;
+        width: 96%;
+        margin: 20rpx;
+        height: 200rpx;
+        background-color: #fff;
+        border-radius: 16rpx;
+    }
+
+    .sex {
+        display: flex;
+        padding: 0 20px;
+        justify-content: space-between;
+        line-height: 100rpx;
+        cursor: pointer;
+    }
+
+    .cuIcon-check {
+        color: #0081ff;
+    }
+
+    .gap {
+        width: 98%;
+        height: 1px;
+        margin: 0 auto;
+        background: #f0f0f0;
+    }
 </style>
