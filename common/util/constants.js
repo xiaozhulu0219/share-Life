cdeccdabc2cb14da038faa06abd12b10f3816902
allentoup@ <allentoup@163.com>
2023-07-31 15:08:08 +0800

    丰富一下违规词库

--- common/util/constants.js
+++ common/util/constants.js
@@ -9,12 +9,20 @@ const STORAGE_OPTIONS = {
 };
 
 export const keyWords = [
-  '尼玛', '我操你妈', '操你妈', '我草泥马', '艹', '操', '草', '我擦', '尼玛批', 'nm', 'wocaonima', 'cao', 'ca', 'woca', 'nmp', 'nimapi', 'fuck',
-  '傻逼', '沙北', '傻杯', '鸡巴', '鸡毛', 'sb', 'jb', 'jiba',
-  '习近平', '军队', '国家领导人', '党', '领袖', '政府', 'zf', 'zhengfu', 'xijinping', 'xjp', 'gjldr', 'guojialingdaoren', 'guojia', 'lingdaoren', 'dang', 'lingxiu',
-  '毒品', '黄赌毒', '色情', '迷药', '杀', '强奸', '诱奸', '死', 'dupin', 'hdd', 'huangdudu', 'seqing', 'sese', 'se', 'miyao', 'qj', 'qiangjian', 'youjian', 'si',
-  '尼格', '黑鬼', 'nige', 'heigui',
-  '史无前例', '前无古人', '史无前例', '前无古人', '史无前例', '前无古人',
+  '尼玛','尼妈', '我操你妈', '操你妈', '我草泥马', '艹', '操', '草', '我擦', '尼玛批','你妹','尼妹','我去年买了块表','我去你妈',
+  'nm', 'wocaonima', 'cao', 'ca', 'woca', 'nmp', 'nimapi', 'fuck','mmp','wcnm','nm','woqunianmailekuaibiao','wqnmlgb','nmlgb',
+  '傻逼', '沙北','傻悲', '傻杯', '鸡巴', '鸡毛','鸡鸡', '生殖器','阴茎',
+  'sb','shabi','shabei', 'jb', 'jiji','jj','szq','shengzhiqi','yinjing',
+  '习近平', '军队', '国家领导人', '党', '共产党','领袖', '政府', '彭丽媛',
+  'zf', 'zhengfu', 'xijinping', 'xjp', 'gjldr', 'guojialingdaoren', 'guojia', 'lingdaoren', 'dang', 'lingxiu','gongchandang','gcd','pengliyuan',
+  '毒品', '毒药','黄赌毒', '赌博','色情', '迷药', '杀', '强奸', '诱奸', '迷奸','死','血','轮奸','裸体','法论功','法轮大法',
+  'dupin','duyao', 'hdd', 'huangdudu', 'dubo','seqing', 'sese', 'se', 'miyao', 'qj', 'qiangjian', 'youjian','mijian', 'si','xue','lunjian','luoti','falungong','flg','falundafa',
+  '尼格', '黑鬼',
+  'nige', 'heigui',
+  '新冠',
+  'xinguan',
+
+  '史无前例', '前无古人', '史无前例', '前无古人', '史无前例', '前无古人', '史无前例', '前无古人',
   '史无前例', '前无古人', '史无前例', '前无古人', '史无前例', '前无古人', '史无前例', '前无古人',
   '史无前例', '前无古人', '史无前例', '前无古人', '史无前例', '前无古人', '史无前例', '前无古人'
 ];
